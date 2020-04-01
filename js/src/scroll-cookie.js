c999899274485b3fb070a9ef2d9ada073f228c78
itlubber <1830611168@qq.com>
2020-04-01 17:47:05 +0800

    Site updated: 2020-04-01 17:47:04

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 01 2020 17:40:29 GMT+0800 (中国标准时间)
-$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
+// build time:Wed Apr 01 2020 17:45:17 GMT+0800 (中国标准时间)
+$(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
