6f7356e98e8ecfb4c8338e0ff856dd0487e1d019
itlubber <1830611168@qq.com>
2020-04-01 17:30:29 +0800

    Site updated: 2020-04-01 17:30:29

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 01 2020 16:52:40 GMT+0800 (中国标准时间)
+// build time:Wed Apr 01 2020 17:28:33 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/funny.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
