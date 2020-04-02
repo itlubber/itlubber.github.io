799bb035d8aa533cda0fefa9088dd481504dd648
itlubber <1830611168@qq.com>
2020-04-02 22:40:20 +0800

    Site updated: 2020-04-02 22:40:20

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -154,8 +154,10 @@ function loadWidget(config) {
 	}
 
 	(function initModel() {
-		let modelId = localStorage.getItem("modelId"),
-			modelTexturesId = localStorage.getItem("modelTexturesId");
+		// let modelId = localStorage.getItem("modelId"),
+		// 	modelTexturesId = localStorage.getItem("modelTexturesId");
+		let modelId = 6;
+		modelTexturesId = 2;
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			modelId = 1; // 模型 ID
