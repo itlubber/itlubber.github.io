0a4d1e6d0a830dd67cee1cf05eeb985b4a343856
itlubber <1830611168@qq.com>
2021-06-30 16:45:08 +0800

    Site updated: 2021-06-30 16:45:08

--- js/arknights.js
+++ js/arknights.js
@@ -1,6 +1,5 @@
-"use strict";
-var dust = /** @class */ (function () {
-    function dust() {
+class dust {
+    constructor() {
         this.x = 50;
         this.y = 50;
         this.vx = Math.random() * 2 + 2;
@@ -13,64 +12,59 @@ var dust = /** @class */ (function () {
         this.radiusY = Math.random() * 3;
         this.rotation = Math.PI * Math.floor(Math.random() * 2);
     }
-    return dust;
-}());
-var canvasDust = /** @class */ (function () {
-    function canvasDust(canvasID) {
-        var _this = this;
+}
+class canvasDust {
+    constructor(canvasID) {
         this.width = 300;
         this.height = 300;
         this.dustQuantity = 50;
         this.dustArr = [];
-        var canvas = document.getElementById(canvasID);
+        const canvas = document.getElementById(canvasID);
         if (canvas) {
             this.canvas = canvas;
             this.ctx = canvas.getContext('2d');
             this.build();
-            window.addEventListener('resize', function () { return _this.resize(); });
+            window.addEventListener('resize', () => this.resize());
         }
         else {
             throw new Error('canvasID 无效');
         }
     }
-    canvasDust.prototype.build = function () {
-        var _this = this;
+    build() {
         this.resize();
         if (this.ctx) {
-            var point = canvasDust.getPoint(this.dustQuantity);
-            for (var _i = 0, point_1 = point; _i < point_1.length; _i++) {
-                var i = point_1[_i];
-                var dustObj = new dust();
+            const point = canvasDust.getPoint(this.dustQuantity);
+            for (let i of point) {
+                const dustObj = new dust();
                 this.buildDust(i[0], i[1], dustObj);
                 this.dustArr.push(dustObj);
             }
-            setInterval(function () {
-                _this.play();
+            setInterval(() => {
+                this.play();
             }, 40);
         }
-    };
-    canvasDust.prototype.play = function () {
+    }
+    play() {
         var _a;
-        var dustArr = this.dustArr;
+        const dustArr = this.dustArr;
         (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.width, this.height);
-        for (var _i = 0, dustArr_1 = dustArr; _i < dustArr_1.length; _i++) {
-            var i = dustArr_1[_i];
+        for (let i of dustArr) {
             if (i.x < 0 || i.y < 0) {
-                var x = this.width;
-                var y = Math.floor(Math.random() * window.innerHeight);
+                const x = this.width;
+                const y = Math.floor(Math.random() * window.innerHeight);
                 i.x = x;
                 i.y = y;
                 this.buildDust(x, y, i);
             }
             else {
-                var x = i.x - i.vx;
-                var y = i.y - i.vy;
+                const x = i.x - i.vx;
+                const y = i.y - i.vy;
                 this.buildDust(x, y, i);
             }
         }
-    };
-    canvasDust.prototype.buildDust = function (x, y, dust) {
-        var ctx = this.ctx;
+    }
+    buildDust(x, y, dust) {
+        const ctx = this.ctx;
         if (x)
             dust.x = x;
         if (y)
@@ -86,11 +80,11 @@ var canvasDust = /** @class */ (function () {
             ctx.fillStyle = dust.color;
             ctx.fill();
         }
-    };
-    canvasDust.prototype.resize = function () {
-        var canvas = this.canvas;
-        var width = window.innerWidth;
-        var height = window.innerHeight;
+    }
+    resize() {
+        const canvas = this.canvas;
+        const width = window.innerWidth;
+        const height = window.innerHeight;
         this.width = width;
         this.height = height;
         this.dustQuantity = Math.floor((width + height) / 38);
@@ -98,51 +92,47 @@ var canvasDust = /** @class */ (function () {
             canvas.width = width;
             canvas.height = height;
         }
-    };
-    canvasDust.getPoint = function (number) {
-        if (number === void 0) { number = 1; }
-        var point = [];
-        for (var i = 0; i < number; i++) {
-            var x = Math.floor(Math.random() * window.innerWidth);
-            var y = Math.floor(Math.random() * window.innerHeight);
+    }
+    static getPoint(number = 1) {
+        let point = [];
+        for (let i = 0; i < number; i++) {
+            const x = Math.floor(Math.random() * window.innerWidth);
+            const y = Math.floor(Math.random() * window.innerHeight);
             point.push([x, y]);
         }
         return point;
-    };
-    return canvasDust;
-}());
-var index = /** @class */ (function () {
-    function index() {
-        var _this = this;
+    }
+}
+class index {
+    constructor() {
         this.index = [];
         this.headerLink = document.getElementsByClassName("headerlink");
         this.tocLink = document.getElementsByClassName("toc-link");
         this.postContent = document.getElementById("post-content");
         this.article = document.getElementsByTagName("article")[0];
         if (this.article) {
-            this.article.addEventListener("scroll", function () {
-                for (var i = 0; i < _this.headerLink.length; i++) {
-                    var link = _this.headerLink.item(i);
+            this.article.addEventListener("scroll", () => {
+                for (let i = 0; i < this.headerLink.length; i++) {
+                    const link = this.headerLink.item(i);
                     if (link) {
-                        _this.index.push(link.getBoundingClientRect().top);
+                        this.index.push(link.getBoundingClientRect().top);
                     }
                 }
-                for (var i in _this.index) {
-                    var item = _this.tocLink.item(Number(i));
+                for (let i in this.index) {
+                    const item = this.tocLink.item(Number(i));
                     item.classList.remove('active');
                 }
-                for (var i in _this.index) {
-                    var item = _this.tocLink.item(Number(i));
-                    if (_this.index[i] > 0) {
+                for (let i in this.index) {
+                    const item = this.tocLink.item(Number(i));
+                    if (this.index[i] > 0) {
                         item.classList.add('active');
                         break;
                     }
                 }
-                _this.index = [];
+                this.index = [];
             });
         }
     }
-    return index;
-}());
+}
 new canvasDust('canvas-dust');
 new index();
